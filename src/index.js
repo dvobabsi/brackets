9e51a787edbc3a2061139810f1e4b61bcaaa00dd
dvobabsi <dvobabsi@gmail.com>
2023-02-09 21:50:47 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // const OPEN_BRACKETS = ['(', '{'];
+  // const BRACKETS_PAIR = {
+  //   [')']: '(',
+  //   ['}']: '{',
+  // };
+  const sumBracketsConfig = bracketsConfig.map((item) => {
+    return `${item[0]}${item[1]}`;
+  });
+  let steLength = str.length;
+
+  while (str.length) {
+    sumBracketsConfig.forEach((item) => {
+      str = str.replaceAll(item, "");
+    });
+    if (str.length === steLength) {
+      return false;
+    }
+
+    steLength = str.length;
+  }
+  return true;
+
+
 }
